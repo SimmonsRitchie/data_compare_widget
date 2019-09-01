04d308b5531c7f714596e312cf090c03a3c11c13
Daniel SR <daniel.simmons.ritchie@gmail.com>
2019-09-01 11:50:23 -0400

    improve comments

--- src/components/infoBox/infoBox.js
+++ src/components/infoBox/infoBox.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { filterArray, averageData, getUniqueVals } from "./../../utils/handleData"
 import InfoGroup from "./infoGroup"
+import InfoIcon from './infoIcon'
 
 class InfoBox extends React.Component {
   render() {
@@ -11,14 +12,17 @@ class InfoBox extends React.Component {
       groupBy,
       fields,
     } = this.props;
+    // Filter data based on child input
     const filteredData = filterArray(data, childOptionsKey, selection)
+    // Get data groups, eg. Algebra 1, Literature, etc.
     const groups = getUniqueVals(filteredData, groupBy)
+    // Average
     const stats = averageData({data: filteredData, groupBy, fields})
     const title = filteredData.length > 0 ? filteredData[0][childOptionsKey] : ""
-    console.log(stats)
     return (
       <div className="box info-box__container">
       <div className="info-box__title">{title}</div>
+      <InfoIcon icon="school" />
       {stats && (
         <InfoGroup 
         groups={groups}
