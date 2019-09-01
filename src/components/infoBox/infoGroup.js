900b7effe58192bee7e4d69100a4f05a64be2f72
Daniel SR <daniel.simmons.ritchie@gmail.com>
2019-09-01 11:50:09 -0400

    intergrate bulma table

--- src/components/infoBox/infoGroup.js
+++ src/components/infoBox/infoGroup.js
@@ -1,23 +1,27 @@
 import React from "react";
 
-
-const InfoGroup = ({groups, fields, data}) => (
+const InfoGroup = ({ groups, fields, data }) => (
   <div className="">
-  {groups.map(group => {
-    return (
-      <div key={group} className="info-group__inner-container">
-      <div className="info-group__title">{group}</div>
-      <div className="info-group__body">
-        {
-          fields.map((field, idx) => {
-            return <div key={field + idx}>{`${field}: ${data[group][field]}`}</div>
-          })
-        }
-      </div>
-      </div>
-    )
-  })}
-</div>
-)
+    {groups.map(group => {
+      return (
+        <div key={group} className="info-group__inner-container">
+          <div className="info-group__title">{group}</div>
+          <table className="table info-group__body">
+            <tbody>
+              {fields.map((field, idx) => {
+                return (
+                  <tr key={field + idx}>
+                    <td>{field}</td>
+                    <td>{data[group][field]}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      );
+    })}
+  </div>
+);
 
-export default InfoGroup
+export default InfoGroup;
