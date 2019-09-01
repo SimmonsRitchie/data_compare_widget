d2c6c40beda6af31889d1c4c47a5bdf995517b73
Daniel SR <daniel.simmons.ritchie@gmail.com>
2019-09-01 11:48:29 -0400

    add fontawesome

--- src/app.js
+++ src/app.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './styles/styles.scss';
 import Main from './components/Main'
 import Loader from './components/Loader'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faSchool, faExclamation } from '@fortawesome/free-solid-svg-icons'
+
+// BUILD FONT AWESOME LIB
+library.add(faSchool, faExclamation)
 
 // RENDERAPP
 // Function below checks to see whether we have already rendered the page
