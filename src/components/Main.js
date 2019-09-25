4f54dc938e918305def91d32d0dc366c9be2fd29
Daniel SR <daniel.simmons.ritchie@gmail.com>
2019-09-25 19:07:07 -0400

    change from class component to func component, add hook

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,35 +1,26 @@
-import React from "react";
+import React, {useEffect} from "react";
 import Header from "./Header";
 import Body from "./Body";
 import Footer from "./Footer";
 
 import {pymSendHeight} from '../utils/handlePym'
 
-class Main extends React.Component {
+const Main = (props) => {
 
-  componentDidMount() {
-    // This is intended to fix bug where app is clipped at bottom
-    // on initial load.
+  useEffect(() => {
     pymSendHeight({timeout: 500})
-  }
+  }, []);
 
-  componentDidUpdate() {
-    // Because our app changes height based on displayed content, we 
-    // update the iframe height after DOM elements have been updated.
-    pymSendHeight()
-  }
-
-  render() {
     return (
       <div className="container__outer">
         <div className="container__inner">
           <Header />
-          <Body data={this.props.data}/>
+          <Body data={props.data}/>
           <Footer />
         </div>
       </div>
     );
   }
-}
+
 
 export default Main;
